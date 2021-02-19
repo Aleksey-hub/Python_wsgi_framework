2d5428146aa0013d0f73c652c2e1ae95005f7d9f
Aleksey Ostryakov <ostrav89@yandex.ru>
2021-02-19 19:20:05 +0500

    Added urllib.parse.unquote.

--- fwsgi/fwsgi.py
+++ fwsgi/fwsgi.py
@@ -1,3 +1,6 @@
+import urllib.parse
+
+
 def view_404_page(request):
     print(request)
     return '404 Not Found', [b'404 Page not found']
@@ -18,16 +21,22 @@ class Aplication:
         if request_method == 'GET':
             print('GET запрос')
             data_str = environ['QUERY_STRING']
+            # декодируем urlencoded в utf-8
+            data_str = urllib.parse.unquote(data_str)
             data = self.pars_data(data_str)
-            print(f'Сообщение от {data["email"]}\n'
-                  f'Тема: {data["theme"]}\n'
-                  f'Сообщение: {data["message"]}')
+            if data:
+                print(f'Сообщение от {data["email"]}\n'
+                      f'Тема: {data["theme"]}\n'
+                      f'Сообщение: {data["message"]}')
         elif request_method == 'POST':
             print('POST запрос')
             # Получаем данные POST-запроса в виде байт строки
             data_bytes = self.get_wsgi_input_bytes(environ)
             # декодируем в str
             data_str = data_bytes.decode(encoding='utf-8')
+            # декодируем urlencoded в utf-8
+            data_str = urllib.parse.unquote(data_str)
+            print(data_str)
             # преобразуем str в dict
             data = self.pars_data(data_str)
 
@@ -48,16 +57,16 @@ class Aplication:
         start_response(response, [('Content-type', 'text/html')])
         return page
 
-
     def get_wsgi_input_bytes(self, env):
         '''Получение данных POST запроса'''
         data = env['wsgi.input'].read()
         return data
 
-    def pars_data(self, data:str):
+    def pars_data(self, data: str):
         result = {}
-        data = data.split('&')
-        for item in data:
-            key, val = item.split('=')
-            result[key] = val
+        if data:
+            data = data.split('&')
+            for item in data:
+                key, val = item.split('=')
+                result[key] = val
         return result
